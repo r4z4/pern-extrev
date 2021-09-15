d2cf8323169d5d61f5e2b304d4d0f308a35eb175
r4z4 <85771218+r4z4@users.noreply.github.com>
2021-09-15 10:41:02 -0500

    adding db.js

---
+++ db.js
@@ -0,0 +1,9 @@
+const Pool = require("pg").Pool;
+const pool = new Pool({
+	connectionString: process.env.DATABASE_URL,	// use DATABASE_URL environment variable from Heroku app 
+	ssl: {
+	  rejectUnauthorized: false // don't check for SSL cert
+	}
+  });
+
+module.exports = pool;
